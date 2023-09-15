065414257b94de08e796c778a984469c50e35f4a
GeNiaaz <geniaaz1009@gmail.com>
2023-09-16 01:28:31 +0800

    add 2 tables init config

---
+++ models/products.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize,DataTypes) => {
+
+    const products = sequelize.define('products',{
+
+        product_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: DataTypes.STRING,
+        price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+        },
+        category: DataTypes.STRING,
+        stock_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    });
+
+    return products;
+}
