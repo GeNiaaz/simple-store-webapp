87699d759b34017acf130d3decc6bb511145d92a
GeNiaaz <geniaaz1009@gmail.com>
2023-09-16 17:09:27 +0800

    add create Product API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('./models');
+const { products } = require('./models');
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -13,11 +14,31 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/products', async (req, res) => {
+
+    try {
+        console.log(req);
+        await products.create({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            category: req.body.category,
+            stock_quantity: req.body.stock_quantity,
+        });
+  
+        res.send('product added');
+  
+    } catch (error) {
+      res.json(error);
+    //   console.log(error);
+    }
+  
+  });
 
 db.sequelize.sync().then((res) => {
 
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Online-Store app listening at http://localhost:${port}`);
   })
 
 })
