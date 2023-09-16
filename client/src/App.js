c87ee57d50d8ac0b8c8fa68016a24b0fee5c8b34
GeNiaaz <geniaaz1009@gmail.com>
2023-09-17 00:46:37 +0800

    update main files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import "./App.css";
+import {
+  Route,
+  Switch,
+  BrowserRouter,
+  Redirect,
+  Router,
+} from "react-router-dom";
+import AddProductPage from "./views/AddProductPage";
 
 function App() {
   return (
-    <div className="App">Hello world</div>
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="content">
+            <Switch>
+              <Redirect exact from="/" to="/add" />
+              <Route path="/add" component={AddProductPage} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
