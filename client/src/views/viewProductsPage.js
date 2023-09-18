7da1b1ab6857a729c92cedd961396e0978c1b1a9
GeNiaaz <geniaaz1009@gmail.com>
2023-09-18 23:26:36 +0800

    change ui for viewproducts view

--- client/src/views/viewProductsPage.js
+++ client/src/views/viewProductsPage.js
@@ -1,5 +1,5 @@
 import "./ViewProductsPage.css";
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import Axios from "axios";
 import Navbar from "../components/Navbar";
 
@@ -11,25 +11,39 @@ function ViewProductsPage(props) {
     });
   };
 
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="ViewProductsPage">
       <Navbar />
       <div className="products">
-        <button onClick={getProducts}>Refresh Products</button>
-
-        {ProductsList.map((val, key) => {
-          return (
-            <div className="product">
-              <div>
-                <h3>Name: {val.name}</h3>
-                <h3>Price: ${val.price}</h3>
-                <h3>Description: {val.description}</h3>
-                <h3>Stock_Quantity: {val.stock_quantity}</h3>
-                <h3>Category: {val.category}</h3>
+        <h1>Products</h1>
+        <div className="product-grid">
+          {ProductsList.map((prod, key) => {
+            if (prod.description.length > 90) {
+              prod.description = prod.description.substring(0, 90) + "...";
+            }
+            return (
+              <div className="product">
+                <div className="product-card" key={key}>
+                  <div className="product-header">
+                    <h2>{prod.name}</h2>
+                    <h2>${prod.price}</h2>
+                  </div>
+                  <div className="product-footer">
+                    <mark>{prod.category}</mark>
+                    <h3>Stocks: {prod.stock_quantity}</h3>
+                  </div>
+                  <div className="product-description">
+                    <p>{prod.description}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
