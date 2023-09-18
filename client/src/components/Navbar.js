a21ccbbc753aae41c93d9db307b1eb2dfc67e2e3
GeNiaaz <geniaaz1009@gmail.com>
2023-09-18 22:27:16 +0800

    make navbar more usable

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,7 @@
 import { useRef } from "react";
+import { IoLogIn } from "react-icons/io5";
+import { IoStorefrontSharp } from "react-icons/io5";
+import { IoLogOut } from "react-icons/io5";
 import "./Navbar.css";
 
 function Navbar() {
@@ -6,12 +9,17 @@ function Navbar() {
 
   return (
     <header>
-      <h3>Online Store</h3>
+      <h3>Uncle Ben's Store</h3>
       <nav ref={navRef}>
-        <a href="/products">Products</a>
-        <a href="/edit">Admin</a>
-        <a href="/add">Add Product</a>
-        <a href="/#">Login</a>
+        <a href="/products">
+          <IoStorefrontSharp></IoStorefrontSharp> Products
+        </a>
+        <a href="/">
+          <IoLogIn></IoLogIn> Login
+        </a>
+        <a href="/#">
+          <IoLogOut></IoLogOut> Logout
+        </a>
       </nav>
     </header>
   );
