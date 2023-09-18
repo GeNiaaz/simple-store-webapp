1876f7ac7752d3ca6987a206dbf902a841cff1b7
GeNiaaz <geniaaz1009@gmail.com>
2023-09-18 22:28:48 +0800

    specify lengths for data in db model

--- server/models/products.js
+++ server/models/products.js
@@ -4,28 +4,28 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
-      validate: {
-        min: 0,
-      },
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 255],
+        len: [1, 17],
       },
     },
     description: DataTypes.STRING,
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        len: [1, 7],
+      },
     },
     category: DataTypes.STRING,
     stock_quantity: {
       type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
-        min: 0,
+        len: [3, 255],
       },
     },
   });
