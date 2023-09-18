f8d61e160dc24cfe78ed17a10cdf61cc536b27da
GeNiaaz <geniaaz1009@gmail.com>
2023-09-19 03:35:52 +0800

    edit logs

--- server/models/logs.js
+++ server/models/logs.js
@@ -1,23 +1,21 @@
-module.exports = (sequelize,DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
+  const logs = sequelize.define("logs", {
+    log_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "products",
+        key: "product_id",
+      },
+      allowNull: false,
+    },
+    action: DataTypes.STRING,
+    details: DataTypes.STRING,
+  });
 
-    const logs = sequelize.define('logs',{
-
-        log_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        product_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'products',
-                key: 'product_id'
-            },
-            allowNull: false,
-        },
-        action: DataTypes.STRING,
-        details: DataTypes.STRING,
-    });
- 
-    return logs;
-}
+  return logs;
+};
